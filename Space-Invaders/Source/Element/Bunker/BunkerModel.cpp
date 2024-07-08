2546fff8817bdc50a8dd39c1ddbd3d8e529a0c22
pranaysj <pranaysj3@gmail.com>
2024-07-08 11:33:13 +0530

    Spawn Multiple Barriers

--- Space-Invaders/Source/Element/Bunker/BunkerModel.cpp
+++ Space-Invaders/Source/Element/Bunker/BunkerModel.cpp
@@ -5,7 +5,7 @@ namespace Element {
 	namespace Bunker {
 		BunkerData::BunkerData() {}
 		BunkerData::BunkerData(sf::Vector2f _position) {
-			this->position = _position
+			this->position = _position;
 		}
 	}
 }
