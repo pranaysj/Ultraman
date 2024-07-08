3dcb6899ef211deed033911c32c32639d6aa9562
pranaysj <pranaysj3@gmail.com>
2024-07-08 13:52:39 +0530

    Created Sound Service
    Created SoundService header and source files.

---
+++ Space-Invaders/Header/Sound/SoundService.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "SFML/Audio.hpp"
+
+namespace Sound
+{
+	enum class SoundType
+	{
+		BUTTON_CLICK,
+	};
+
+	class SoundService
+	{
+	private:
+		const int backgroundMusicVolume = 30;
+
+		sf::Music backgroundMusic;
+		sf::Sound soundEffect;
+		sf::SoundBuffer bufferButtonClick;
+
+		void LoadBackgroundMusicFromFile();
+		void LoadSoundFromFile();
+
+	public:
+		void Initialize();
+
+		void PlaySound(SoundType _soundType);
+		void PlayBackgroundMusic();
+	};
---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,28 @@
+#pragma
+#include "../../Header/Sound/SoundService.h"
+#include "../../Header/Global/ServiceLocator.h"
+
+namespace Sound
+{
+	using namespace Global;
+
+	void SoundService::Initialize()
+	{
+	}
+
+	void SoundService::LoadBackgroundMusicFromFile()
+	{
+	}
+
+	void SoundService::LoadSoundFromFile()
+	{
+	}
+
+	void SoundService::PlaySound(SoundType soundType)
+	{
+	}
+
+	void SoundService::PlayBackgroundMusic()
+	{
+	}
+}
