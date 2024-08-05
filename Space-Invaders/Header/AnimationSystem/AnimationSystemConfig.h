444a3dd17120a1438762666272c24e018b64f7b9
pranaysj <pranaysj3@gmail.com>
2024-08-05 10:59:47 +0530

    Create the Data For Your Animations

---
+++ Space-Invaders/Header/AnimationSystem/AnimationSystemConfig.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+
+namespace AnimationSystem {
+	struct AnimationSystemConfig {
+
+		sf::String animationTexturePath;
+
+		float spriteSheetWidth;
+		float spriteSheetHeight;
+
+		float tileWidth;
+		float tileHeight;
+
+		int numberOfAnimationFrames;
+		float frameDuration;
+
+		AnimationSystemConfig() = default;
+
+		AnimationSystemConfig(sf::String _animationTexturePath, float _spriteWidth, float _spriteHeight, float _tileWidth, float _tileHeight, int _numberOfAnimationFrame, int _frameDuration) :
+			animationTexturePath(_animationTexturePath),
+			spriteSheetWidth(_spriteWidth),
+			spriteSheetHeight(_spriteHeight),
+			tileWidth(_tileWidth),
+			tileHeight(_tileHeight),
+			numberOfAnimationFrames(_numberOfAnimationFrame),
+			frameDuration(_frameDuration) {}
+	};
+}
---
+++ Space-Invaders/Header/AnimationSystem/AnimationSystemConfigData.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"../../Header/AnimationSystem/AnimationSystemConfig.h"
+#include"../../Header/Global/Config.h"
+
+namespace AnimationSystem {
+	const AnimationSystemConfig explosionAnimationConfig(Global::Config::explosion_texture_path,70.0f, 80.0f, 14.28f, 20.0f, 7, 0.03f);
+}
